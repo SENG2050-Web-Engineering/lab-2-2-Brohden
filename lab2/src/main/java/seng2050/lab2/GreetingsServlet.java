58c23881965c70a3325c8574fee4cfb2dca61716
Brohden Mcilveen <brohden.mcilveen@uon.edu.au>
2026-02-08 13:26:32 +1100

    Section 3

---
+++ lab2/src/main/java/seng2050/lab2/GreetingsServlet.java
@@ -0,0 +1,34 @@
+package seng2050.lab2;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class GreetingsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html; charset=UTF-8");
+
+        String firstName = req.getParameter("firstName");
+        String lastName = req.getParameter("lastName");
+
+        PrintWriter out = resp.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html lang=\"en\">");
+        out.println("<head><meta charset=\"UTF-8\"><title>Welcome</title></head>");
+        out.println("<body>");
+        out.println("<h1>Welcome</h1>");
+        if (firstName != null && lastName != null) {
+            out.println("<p>Welcome " + firstName + " " + lastName + " to the course web application. Glad you're here.</p>");
+        } else {
+            out.println("<p>Welcome to the course web application. Glad you're here.</p>");
+        }
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
